5bcea5342758b400e26fcbce45c2e0e8e73c5f8a
Roei Rabany <roei5591@gmail.com>
2021-05-02 23:22:16 +0300

    Credorax interview - Final version for production with history support improved design and dark mode.

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -5,7 +5,47 @@ import FormControl from '@material-ui/core/FormControl';
 import InputLabel from '@material-ui/core/InputLabel';
 import Button from '@material-ui/core/Button';
 import converter from 'number-to-words';
+import moment from 'moment';
+import DeleteIcon from '@material-ui/icons/Delete';
 
+interface history {
+  number: string;
+  time: string;
+}
+
+const History = ({history , removeFromHistory , setFromHistory} : {
+  history: history[];
+  removeFromHistory: (index: number) => void
+  setFromHistory: (historyInput: string) => void
+}) => {
+
+const historyList = history.map((item,index) => { return <tr>
+    <td onClick={() => setFromHistory(item.number)}>{item.number}</td>
+    <td>{item.time}</td>
+    <td > 
+     <DeleteIcon onClick={ ()=> removeFromHistory(index)}/> 
+    </td>
+  </tr>
+ }) 
+
+ return (    
+ <div className="history">
+  <span> History</span>
+  
+  <table>
+    <thead>
+      <tr>
+        <th>Number</th>
+        <th>time</th>
+        <th style={{visibility: "hidden"}}>remove</th>
+      </tr> 
+    </thead> 
+    <tbody>
+      {historyList}
+    </tbody>
+ </table>
+</div>
+)}
 
 
 
@@ -17,7 +57,6 @@ const Result = ( {result} : { result: string }) => {
     )
 }
 
-
 function Form() {
 
   const initMessage = "Please enter a number";
@@ -25,13 +64,29 @@ function Form() {
   const [input, setInput] = useState('');
   const [result, setResult] = useState('');
   const [message, setMessage] = useState(initMessage);
- 
-  const handleSubmit = (event : React.FormEvent<HTMLFormElement>) => {
+  const [history , setHistory] = useState<history[]>([]) 
+
+  const removeFromHistory = (index: number) => {
+    setHistory(prev => prev.filter((item , fIndex) => fIndex !== index))}
+
+  //sets the number input on click of number in the history
+  const setFromHistory = (historyInput: string) => {
+    setResult('')
+    setInput(historyInput);
+  } 
+
+    const handleSubmit = (event : React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const parsedInput =  parseInt(input);
+
+    //check if the submitted input is a number
     if( !Number.isNaN(parsedInput)){
       const numbersInWords = converter.toWords(parsedInput);
       setResult(numbersInWords)
+      setHistory(prev => [  {
+        number: input , 
+        time: moment(Date.now()).format('MMMM Do YYYY, hh:mm')
+      } , ...prev])
       setMessage(initMessage);
       setInput('')
     } else {
@@ -41,8 +96,6 @@ function Form() {
   }
 
 
-  
- 
   return (
   <>
     <form noValidate autoComplete="off" onSubmit={handleSubmit}>
@@ -59,6 +112,12 @@ function Form() {
     </form> 
 
     <Result result={result}/>
+
+    <History 
+      history={history} 
+      removeFromHistory={removeFromHistory}
+      setFromHistory={setFromHistory}
+      />
     </>
   );
 }
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,15 +1,37 @@
 import './NavBar.css';
+import Switch from '@material-ui/core/Switch';
 import  { useState } from 'react';
 
 
+const ModeSwitch = () => {
+  const [mode, setMode] = useState(true);
+
+  const handleChange = () => {
+    const body = document.getElementsByTagName("body")[0];
+     body.setAttribute("data-theme" , !mode ? "light" : "dark");
+      setMode(prev => !prev);
+    };
+
+  return (
+    <div className = "mode_switch">
+    <Switch
+      checked={mode}
+      color="default"
+      onChange={handleChange}
+    />
+   </div>
+  )
+}
 
 
 function NavBar() {
 
   return (<>
     <header className="top">
-      <nav className="nav_top">     
+      <nav className="nav_top">  
+      <div className="credit">Created by Roei Rabany</div>   
       </nav>
+      <ModeSwitch/>
     </header>
    <div className="block" ></div>
    </>
